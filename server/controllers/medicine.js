473ebeda1713e691253fd57994a00c5e102d1464
George-Elhamy <104439178+George-Elhamy@users.noreply.github.com>
2023-10-09 10:30:19 +0300

    add/search medicine and view list of medicines

--- server/controllers/medicine.js
+++ server/controllers/medicine.js
@@ -1,6 +1,55 @@
 import mongoose from 'mongoose';
 import MedicineModel from '../models/medicine.js';
 
+
+// add a new medicine with all the details 
+const addMedicine = async (req, res) => {
+  try {
+
+      const {
+          medicineName,
+          price,
+          quantity,
+          ingredients,
+          medicineStatus,
+          description,
+          sales,
+          medicinalUse,
+          path,
+      } = req.body;
+
+      const medicine = new MedicineModel({
+          medicineName,
+          price,
+          quantity,
+          ingredients,
+          description,
+          medicineStatus,
+          sales,
+          medicinalUse,
+          path,
+      });
+      await medicine.save();
+      console.log(medicine);
+      res.status(200).json(medicine);
+  } catch (error) {
+      res.status(400).json({ error: error.message })
+  }
+}
+
+// search a medicine by name 
+const searchByName = async (req, res) => {
+  try {
+      const lowerName = req.body.name.toLowerCase();
+      const medicine = await MedicineModel.find({medicineName: { $regex: new RegExp(lowerName, 'i') }});
+      res.status(200).json(medicine);
+  }
+  catch (error) {
+      res.status(400).json({ error: error.message })
+  }
+}
+
+
 const updateMedicine = async (req, res) => {
   const {
     description,
@@ -26,7 +75,61 @@ const filterMedicine = async (req, res) => {
   }
 }
 
+// this returns name , quantity and sales of all medicines (for pharmacist only) 
+const medicineDetails = async (req, res) => {
+  try {
+      const medicines = await MedicineModel.find();
+      const selectedData = medicines.map((item) => {
+          return {
+            medicineName: item.medicineName,
+            quantity: item.quantity,
+            sales: item.sales
+          };
+        });
+        
+        console.log(selectedData);
+        
+      res.status(200).json(selectedData);
+  }
+  catch (error) {
+      res.status(400).json({ error: error.message })
+  }
+}
+
+
+// list specific details of all medicines 
+const listMedicines = async (req, res) => {
+  try {
+      const medicines = await MedicineModel.find();
+      const selectedData = medicines.map((item) => {
+          return {
+            medicineName: item.medicineName,
+            price: item.price,
+            ingredients: item.ingredients,
+            description: item.description,
+            medicineStatus: item.medicineStatus,
+            medicalUse: item.medicalUse,
+            quantity: item.quantity,
+            path: item.path
+          };
+        });
+        
+        console.log(selectedData);
+        
+      res.status(200).json(selectedData);
+  }
+  catch (error) {
+      res.status(400).json({ error: error.message })
+  }
+}
+
+
+
 export default {
   updateMedicine,
-  filterMedicine
+  filterMedicine,
+  addMedicine,
+  searchByName,
+  medicineDetails, 
+  listMedicines
 }
--- server/routes/medicine.js
+++ server/routes/medicine.js
@@ -5,5 +5,10 @@ const router = express.Router();
 
 router.put("/:id", controllers.updateMedicine);
 router.get("/filter", controllers.filterMedicine);
+router.get("/search" , controllers.searchByName);
+router.get("/listMedicines" , controllers.listMedicines);
+router.get("/medicineDetails" , controllers.medicineDetails);
+router.post("/addMedicine" , controllers.addMedicine);
+
 
 export default router;
