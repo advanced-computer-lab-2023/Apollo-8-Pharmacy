6bf37f89e10b7a0702e0d243902753403ab7586f
youssef Poulis <youssef@youssefs-MacBook-Pro-2.local>
2023-10-11 11:55:19 +0300

    finish sidebar

--- client/src/pages/AddAdmin.jsx
+++ client/src/pages/AddAdmin.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 function AddAdmin() {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
-  
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -28,16 +28,18 @@ function AddAdmin() {
   };
   return (
     <div className="d-flex justify-content-center align-itelms-center vh-100 bg-light">
-                      <div style={{marginLeft:"-15%"}} className="sidebar">
-      <ul>
-        <h2 style={{fontWeight:"bolder"}}>Admin Page</h2>
-        <li style={{margin:"20px"}}><a href="/addAdministratin">Add Admin</a></li>
-        <li style={{margin:"20px"}}><a href="/removeUser">Remove User</a></li>
-        <li style={{margin:"20px"}}><a href="/pharmacists">List of Pharmacist</a></li>
-        <li style={{margin:"20px"}}><a href="/pharmacistsListPending">List of Pharmacist Pending</a></li>
-      </ul>
-    </div>
-    <div className="card m-3 col-12" style={{ width: "80%" }}>
+      <div style={{ marginLeft: "-15%" }} className="sidebar">
+        <ul>
+          <h2 style={{ fontWeight: "bolder" }}>Admin Page</h2>
+          <li style={{ margin: "20px" }}><a href="/addAdministratin">Add Admin</a></li>
+          <li style={{ margin: "20px" }}><a href="/removeUser">Remove User</a></li>
+          <li style={{ margin: "20px" }}><a href="/pharmacists">List of Pharmacist</a></li>
+          <li style={{ margin: "20px" }}><a href="/pharmacistsListPending">List of Pharmacist Pending</a></li>
+          <li style={{ margin: "20px" }}><a href="/patients">Patient List</a></li>
+
+        </ul>
+      </div>
+      <div className="card m-3 col-12" style={{ width: "80%" }}>
         <h2>add Administrator</h2>
         <form action="" onSubmit={handleSubmit}>
           <div className="mb-3">
@@ -53,7 +55,7 @@ function AddAdmin() {
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
-         
+
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Password</strong>
@@ -66,11 +68,11 @@ function AddAdmin() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit"   className="btn btn-success w-100">
+          <button type="submit" className="btn btn-success w-100">
             Add
           </button>
         </form>
-        </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/RemoveUser.jsx
+++ client/src/pages/RemoveUser.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { useEffect } from "react";
 
 function RemoveUser() {
- 
+
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -22,12 +22,12 @@ function RemoveUser() {
         console.error('Error fetching users:', error);
         setLoading(false);
       });
-  }, []); 
- 
+  }, []);
+
   const handleDeleteUser = async (username) => {
     try {
       const response = await axios.delete(`http://localhost:9000/admin/removeUser`, {
-        data: { username },  
+        data: { username },
       });
       if (response.data && response.data.message === 'The User removed successfully') {
         setUsers((prevUsers) => prevUsers.filter((user) => user.username !== username));
@@ -36,67 +36,69 @@ function RemoveUser() {
       console.error('Error deleting user:', error);
     }
   };
-  
-  
+
+
 
   return (
-   <div className="d-flex justify-content-center align-itelms-center vh-100 bg-light">
-                    <div style={{marginLeft:"-15%"}} className="sidebar">
-      <ul>
-        <h2 style={{fontWeight:"bolder"}}>Admin Page</h2>
-        <li style={{margin:"20px"}}><a href="/addAdministratin">Add Admin</a></li>
-        <li style={{margin:"20px"}}><a href="/removeUser">Remove User</a></li>
-        <li style={{margin:"20px"}}><a href="/pharmacists">List of Pharmacist</a></li>
-        <li style={{margin:"20px"}}><a href="/pharmacistsListPending">List of Pharmacist Pending</a></li>
-      </ul>
-    </div>
-    <div className="card m-3 col-12" style={{ width: "80%" }}>
-    <div className="card-header">
+    <div className="d-flex justify-content-center align-itelms-center vh-100 bg-light">
+      <div style={{ marginLeft: "-15%" }} className="sidebar">
+        <ul>
+          <h2 style={{ fontWeight: "bolder" }}>Admin Page</h2>
+          <li style={{ margin: "20px" }}><a href="/addAdministratin">Add Admin</a></li>
+          <li style={{ margin: "20px" }}><a href="/removeUser">Remove User</a></li>
+          <li style={{ margin: "20px" }}><a href="/pharmacists">List of Pharmacist</a></li>
+          <li style={{ margin: "20px" }}><a href="/pharmacistsListPending">List of Pharmacist Pending</a></li>
+          <li style={{ margin: "20px" }}><a href="/patients">Patient List</a></li>
+
+        </ul>
+      </div>
+      <div className="card m-3 col-12" style={{ width: "80%" }}>
+        <div className="card-header">
           <h2>Remove User</h2>
         </div>
         <div className="card-body">
-        {loading ? (
+          {loading ? (
             <p>Loading...</p>
           ) : (
             <table className="table table-striped">
-         <thead className="table-dark">
-            <tr>
-                <th>
+              <thead className="table-dark">
+                <tr>
+                  <th>
                     username
-                </th>
-                <th>
+                  </th>
+                  <th>
                     type
-                </th>
-                <th></th>
-            </tr>
-            
-        </thead>
-        <tbody>
-        {users.length > 0 ? (
-    users.map((user) => (
-      <tr key={user.username}>
-        <td>{user.username}</td>
-        <td>{user.type}</td>
-        <td> 
-          <button  className="btn btn-success"onClick={() => handleDeleteUser(user.username)}>
-            Delete
-          </button>
-        </td>
-      </tr>
-    ))
-  ) : (
-    <tr>
-      <td colSpan="3">No users found</td>
-    </tr>
-  )}
-            
-        
-        </tbody>
+                  </th>
+                  <th></th>
+                </tr>
+
+              </thead>
+              <tbody>
+                {users.length > 0 ? (
+                  users.map((user) => (
+                    <tr key={user.username}>
+                      <td>{user.username}</td>
+                      <td>{user.type}</td>
+                      <td>
+                        <button className="btn btn-success" onClick={() => handleDeleteUser(user.username)}>
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="3">No users found</td>
+                  </tr>
+                )}
+
+
+              </tbody>
             </table>
-     )}
-   </div>
-   </div>
-   </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
