dd70711f2a244e49855822e29ce4081ff1e77b5a
sarah <127792064+sarahhihab@users.noreply.github.com>
2023-11-11 13:48:14 +0200

    Update order.js

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,8 +2,8 @@ import express from "express";
 import controllers from "../controllers/order.js";
 
 const router = express.Router();
-router.post("/:id", controllers.addOrder);
-router.get("/:id", controllers.viewOrderDetails);
-router.post("/:id/cancel", controllers.cancelOrder);
+router.post("/:id/add", controllers.addOrder);
+router.get("/:id/:orderId", controllers.viewOrderDetails);
+router.post("/:id/:orderId/cancel", controllers.cancelOrder);
 
 export default router;
