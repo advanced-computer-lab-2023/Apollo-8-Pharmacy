40db5d5b5742d861ea8e74b4d5da1b761e22d0d5
helenmouris <helenmouris3@gmail.com>
2023-11-14 15:10:47 +0200

    fix medicine list

--- server/controllers/patient.js
+++ server/controllers/patient.js
@@ -84,7 +84,7 @@ const getPatientById = async (req, res) => {
 const addAddressToPatient = async (req, res) => {
   try {
     const patientId = req.params.id;
-    const {newAddress} = req.body;
+    const { newAddress } = req.body;
     const patient = await PatientModel.findById(patientId);
 
     if (!patient) {
@@ -102,17 +102,21 @@ const addAddressToPatient = async (req, res) => {
 
 
 const addToCart = async (req, res) => {
-  const patientId = '65212c32f90a57e39e26a1c2';
+  //const patientId = '65212c32f90a57e39e26a1c2';
   const { medicineId, quantity } = req.body;
 
   try {
-    const patient = await PatientModel.findById(patientId);
+    const patient = await PatientModel.findOne({ user: res.locals.userId });
+    const patientId = patient._id
 
     if (!patient) {
       return res.status(404).json({ error: 'Patient not found' });
     }
 
-    const existingCartItem = patient.cart.find(item => item.medicine.equals(medicineId));
+    const existingCartItem = patient.cart.find(item => {
+      console.log(item)
+      item.medicine.equals(medicineId)
+    });
 
     if (existingCartItem) {
       existingCartItem.quantity += quantity;
@@ -132,7 +136,10 @@ const viewCart = async (req, res) => {
   const patientId = req.params.id;
 
   try {
-    const patient = await PatientModel.findById(patientId).populate('cart.medicine');
+    const patient = await PatientModel.findOne({ user: res.locals.userId }).populate('cart.medicine');
+    console.log(res.locals.userId)
+    console.log(patient)
+    //const patient = await PatientModel.findById(patientId).populate('cart.medicine');
 
     if (!patient) {
       return res.status(404).json({ error: 'Patient not found' });
@@ -197,7 +204,7 @@ const incMedicine = async (req, res) => {
       console.log(cartItem.quantity)
 
       if (medicine.quantity > cartItem.quantity) {
-        
+
         cartItem.quantity += 1;
       } else {
         return res.status(404).json({ error: 'sorry we do not have enough amount of the medicine' });
@@ -260,22 +267,22 @@ const getCartTotal = async (req, res) => {
   const patientId = req.params.id;
   const patient = await PatientModel.findById(patientId);
 
-    if (!patient) {
-      return res.status(404).json({ error: 'Patient not found' });
-    }
-    const cartItems = patient.cart;
-    let total = 0;
+  if (!patient) {
+    return res.status(404).json({ error: 'Patient not found' });
+  }
+  const cartItems = patient.cart;
+  let total = 0;
 
-    for (const cartItem of cartItems) {
-      const medicine = await MedicineModel.findById(cartItem.medicine);
-      console.log(medicine);
-      total += medicine.price * cartItem.quantity;
-      console.log(medicine.price);
-      console.log(cartItem.quantity);
-  
-    }
+  for (const cartItem of cartItems) {
+    const medicine = await MedicineModel.findById(cartItem.medicine);
+    console.log(medicine);
+    total += medicine.price * cartItem.quantity;
+    console.log(medicine.price);
+    console.log(cartItem.quantity);
 
-    res.status(200).json(total);
+  }
+
+  res.status(200).json(total);
 };
 const cancelOrder = async (req, res) => {
   const patientId = req.params.id;
@@ -303,7 +310,7 @@ const cancelOrder = async (req, res) => {
 
 const updateWallet = async (req, res) => {
   try {
-    const { patientId , paymentAmount } = req.body;
+    const { patientId, paymentAmount } = req.body;
     const patient = await PatientModel.findById(patientId);
     console.log(patient);
     patient.wallet += paymentAmount;
@@ -327,8 +334,8 @@ export default {
   incMedicine,
   decMedicine,
   viewOrderDetails,
-  cancelOrder, 
-  addAddressToPatient, 
-  updateWallet, 
+  cancelOrder,
+  addAddressToPatient,
+  updateWallet,
   getCartTotal
 }
--- server/routes/patient.js
+++ server/routes/patient.js
@@ -1,19 +1,20 @@
 import express from "express";
 import controllers from "../controllers/patient.js";
 import Auth from "../Authentication/login.js"
+import Middle from "../Authentication/Middleware.js"
 const router = express.Router();
 //login
-router.post("/patientLogin",Auth.loginPatient)
+router.post("/patientLogin", Auth.loginPatient)
 
 // to test this send a post request to this route: http://localhost:9000/patient
 router.post("/", controllers.createPatient);
 router.get("/", controllers.getPatients);
 router.get("/:id", controllers.getPatientById);
-router.post('/addToCart', controllers.addToCart);
-router.get('/:id/viewCart', controllers.viewCart);
+router.post('/addToCart', Middle.requireAuthPatient, controllers.addToCart);
+router.get('/:id/viewCart', Middle.requireAuthPatient, controllers.viewCart);
 router.delete('/:id/removeFromCart', controllers.removeFromCart);
 router.put('/:id/incMed', controllers.incMedicine);
-router.put('/:id/addAddress' , controllers.addAddressToPatient);
+router.put('/:id/addAddress', controllers.addAddressToPatient);
 router.put('/updateWallet', controllers.updateWallet);
 router.put('/:id/decMed', controllers.decMedicine);
 router.get('/:id/orders/:orderId', controllers.viewOrderDetails);
